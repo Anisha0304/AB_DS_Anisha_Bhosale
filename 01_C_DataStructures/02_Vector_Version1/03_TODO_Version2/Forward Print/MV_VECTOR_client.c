7f2cf2042b0296fa334f4e2ce3002777edb0cc4e
Anisha0304 <anishabhosale003@gmail.com>
2022-11-23 23:41:54 +0530

    23/11/2022

---
+++ 01_C_DataStructures/02_Vector_Version1/03_TODO_Version2/Forward Print/MV_VECTOR_client.c
@@ -0,0 +1,56 @@
+//Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+/**
+ * @file	client.c
+ * @author	MarshallingVoid	(marshallingvoid@gmail.com)
+ * @brief	This file contains the client side usage of DOUBLY CIRCULAR LINKED LIST
+ * @date	06/11/2022
+ * @version	1.0
+ * 
+ * @copyright	Copyrights (c) 2022
+ */
+
+
+#include "MV_VECTOR.h"
+p_mv_vector_t pvector =NULL;
+void print_data(p_mv_vector_t pvector)
+{
+	fprintf(stdout,"\n{START}-");
+	for(long le=0;le<mv_vector_size(pvector); ++le)
+	{
+		fprintf(stdout, "{%lld}-",(long long)mv_vector_data_at(pvector,le));
+	}
+	fprintf(stdout, "{END}\n");
+}
+int main(void)
+{
+	pvector=create_vector();
+	mv_vector_push_back(pvector,(data_t)100);
+	mv_vector_push_back(pvector,(data_t)200);
+	mv_vector_push_back(pvector,(data_t)300);
+	mv_vector_push_back(pvector,(data_t)400);
+	print_data(pvector);
+	printf("pvector size= %llu\n",mv_vector_size(pvector));
+	printf("pvector capacity =%llu\n",mv_vector_capacity(pvector));
+	mv_vector_push_back(pvector, (data_t)500);
+    mv_vector_push_back(pvector,(data_t)600);
+	mv_vector_push_back(pvector,(data_t)700);
+	mv_vector_push_back(pvector,(data_t)800);
+mv_vector_push_back(pvector,(data_t)900);
+mv_vector_push_back(pvector,(data_t)1000);
+print_data(pvector);
+printf("pvector size = %llu\n", mv_vector_size(pvector));
+printf("pvector Capacity = %llu\n", mv_vector_capacity(pvector));
+mv_vector_pop_back(pvector);
+mv_vector_pop_back(pvector);
+mv_vector_pop_back(pvector);
+print_data(pvector);
+printf("pvector size = %llu\n", mv_vector_size(pvector));
+printf("pvector Capacity = %llu\n", mv_vector_capacity(pvector));
+mv_vector_destroy(&pvector);
+exit(0);
+}
+
+
