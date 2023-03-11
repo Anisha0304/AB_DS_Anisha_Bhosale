ba84a3adcb1c7450945a9d2275b381ae75f8866a
Anisha0304 <anishabhosale003@gmail.com>
2023-03-11 12:47:22 +0530

    11-03-2023

--- 05_C_Algorithms/RB Tree/client.c
+++ 05_C_Algorithms/RB Tree/client.c
@@ -1,50 +1,49 @@
 /**
  * file:	client.c
- * author:	MarshallingVoid (marshallingvoid@gmail.com)
+ * author:	AnishaBhosale (anishabhosale003@gmail.com)
  * breif:	All Interface function call needed to be used by client
- * date:	15th Feb, 2023
+ * date:	04 march, 2023
  * version:	1.0 
  */
 
 
-// Headers
-#include	<stdio.h>
-#include	<stdlib.h>
-
-
-#include	"mv_rbt.h"
-
-
-tree_t	tree = NULL;
-
-status_t	compare_data(data_t data1, data_t data2)
+//Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include "mv_rbt.h"
+tree_t	tree =NULL;
+status_t compare_data(data_t data1,data_t data2)
 {
-	// Code
-	if( (long long)data1 == (long long)data2 )
-		return(STATUS_EQUAL);
-	else if( (long long)data1 < (long long)data2 )
-		return(STATUS_LESSER);
-	else 
-		return(STATUS_GREATER);
-}
 
-void	show_data(data_t data)
+//Code
+
+if((long long)data1 ==(long long)data2)
+return(STATUS_EQUAL);
+else if((long long)data1 < (long long)data2)
+return(STATUS_LESSER);
+else 
+return(STATUS_GREATER);
+}
+void show_data(data_t data)
 {
-	// Code
-	fprintf(stdout, "%lld", (long long)data);
-}
 
-void	delete_data(data_t data)
+//Code
+
+fprintf(stdout,"%lld",(long long)data);
+}
+void delete_data(data_t data)
 {
-	// Code
-	fprintf(stdout, "%lld", (long long)data);
-}
 
+//Code
+
+fprintf(stdout,"%lld",(long long)data);
+}
 int main(void)
 {
-	// Code
 
-	tree = create_tree();
+//Code
+
+tree =create_tree();
 
 /*	mv_rbt_insert(tree, (data_t)(long long)500, compare_data);
 	mv_rbt_insert(tree, (data_t)(long long)250, compare_data);
@@ -63,37 +62,28 @@ int main(void)
 	mv_rbt_insert(tree, (data_t)(long long)980, compare_data);
 	mv_rbt_insert(tree, (data_t)(long long)930, compare_data);
 */
-	mv_rbt_insert(tree, (data_t)(long long)50, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)25, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)100, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)10, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)20, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)120, compare_data);
-	mv_rbt_insert(tree, (data_t)(long long)105, compare_data);
 
-
-	mv_rbt_inorder_recursive(tree, show_data);
-	mv_rbt_inorder_non_recursive(tree, show_data);
-
-	mv_rbt_preorder_recursive(tree, show_data);
-	mv_rbt_preorder_non_recursive(tree, show_data);
-	
-	mv_rbt_postorder_recursive(tree, show_data);
-	mv_rbt_postorder_non_recursive(tree, show_data);
-
-	delete_data( mv_rbt_remove(tree, (data_t)(long long)500, compare_data) );
-	printf("\n");
-
-	mv_rbt_inorder_recursive(tree, show_data);
-	mv_rbt_inorder_non_recursive(tree, show_data);
-
-	mv_rbt_preorder_recursive(tree, show_data);
-	mv_rbt_preorder_non_recursive(tree, show_data);
-	
-	mv_rbt_postorder_recursive(tree, show_data);
-	mv_rbt_postorder_non_recursive(tree, show_data);
-
-	mv_rbt_destroy_non_recursive(&tree, delete_data);
-
-	exit(SUCCESS);
+mv_rbt_insert(tree,(data_t)(long long)50,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)25,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)100,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)10,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)20,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)120,compare_data);
+mv_rbt_insert(tree,(data_t)(long long)105,compare_data);
+mv_rbt_inorder_recursive(tree,show_data);
+mv_rbt_inorder_non_recursive(tree,show_data);
+mv_rbt_preorder_recursive(tree,show_data);
+mv_rbt_preorder_non_recursive(tree,show_data);
+mv_rbt_postorder_recursive(tree,show_data);
+mv_rbt_postorder_non_recursive(tree,show_data);
+delete_data( mv_rbt_remove(tree,(data_t)(long long)500,compare_data));
+printf("\n");
+mv_rbt_inorder_recursive(tree,show_data);
+mv_rbt_inorder_non_recursive(tree,show_data);
+mv_rbt_preorder_recursive(tree,show_data);
+mv_rbt_preorder_non_recursive(tree,show_data);
+mv_rbt_postorder_recursive(tree,show_data);
+mv_rbt_postorder_non_recursive(tree,show_data);
+mv_rbt_destroy_non_recursive(&tree,delete_data);
+exit(SUCCESS);
 }
--- 05_C_Algorithms/RB Tree/mv_dcll.h
+++ 05_C_Algorithms/RB Tree/mv_dcll.h
@@ -1,10 +1,10 @@
 
 /**
  * @file	mv_dcll.h
- * @author	MarshallingVoid	(marshallingvoid@gmail.com)
+ * @author	AnishaBhosale	(anishabhosale003@gmail.com)
  * @breif	This file contains all the interface function declarations and 
  * 			type definitions required for DOUBLY CIRCULAR LINKED LIST  
- * @date	06/11/2022
+ * @date	04/03/2023
  * @version	1.0
  * 
  * @copyright	Copyright (c) 2022
@@ -14,63 +14,57 @@
 #ifndef	_MV_DCLL_H
 #define	_MV_DCLL_H
 
-//	Constant Literals
+//Constant Literals
+
 #ifndef	SUCCESS	
 #define	SUCCESS 0
 #endif
-
 #ifndef	FAILURE
 #define	FAILURE	1
 #endif	
 
 //	Enums
+
 enum
 {
-	FALSE = 0,
-	TRUE
+FALSE =0,
+TRUE
 };
 
 //	struct type declarations and typedefs
 
 struct 	list_node;
-
 typedef	struct list_node	node_t;
 typedef	struct list_node*	p_node_t;
 typedef	struct list_node**	pp_node_t;
-
 struct mv_doubly_circular_list;
-
 typedef struct mv_doubly_circular_list		mv_doubly_list_t;
 typedef	struct mv_doubly_circular_list*		p_mv_doubly_list_t;
 typedef	struct mv_doubly_circular_list**	pp_mv_doubly_list_t;
-
 typedef	mv_doubly_list_t	mv_dcll_t;
 typedef	p_mv_doubly_list_t	p_mv_dcll_t;
 typedef	pp_mv_doubly_list_t	pp_mv_dcll_t;
-
 typedef	void*	data_t;
 typedef	int 	ret_t;
-
 typedef	char	bool_t;
-
 typedef		ret_t (*COMPAREDATAPROC)(data_t, data_t);
 typedef		void (*SHOWDATAPROC)(data_t);
 typedef		void (*DELETEDATAPROC)(data_t);
 
 //	Struct Type Definitions
+
 struct list_node
 {
-	data_t	key;
-	p_node_t 	p_next;
-	p_node_t 	p_prev;
+data_t	key;
+p_node_t 	p_next;
+p_node_t 	p_prev;
 };
 #define	SIZE_NODE	(sizeof(node_t))
-
 struct 	mv_doubly_circular_list
 {
-	p_node_t p_head;
-	p_node_t p_tail;
-	size_t	size;
+p_node_t p_head;
+p_node_t p_tail;
+size_t	size;
 };
 #define	SIZE_DCLL	(sizeof(mv_dcll_t))
 
@@ -81,6 +75,7 @@ struct 	mv_doubly_circular_list
  * @params	
  * @returns		pointer to the created list object 
  */
+
 extern p_mv_dcll_t	create_doubly_circular_list();
 
 //	List Data Insertion Functions
@@ -91,7 +86,8 @@ extern p_mv_dcll_t	create_doubly_circular_list();
  * @returns		status of insertion
  * 				SUCCESS if successful / FAILURE if unsuccessful
  */
-extern ret_t mv_dcll_insert_back(p_mv_dcll_t plist, data_t new_data);
+
+extern ret_t mv_dcll_insert_back(p_mv_dcll_t plist,data_t new_data);
 
 /**
  * @function 	This function will add a new data to the FRONT of the list
@@ -100,7 +96,8 @@ extern ret_t mv_dcll_insert_back(p_mv_dcll_t plist, data_t new_data);
  * @returns		status of insertion
  * 				SUCCESS if successful / FAILURE if unsuccessful
  */
-extern ret_t mv_dcll_insert_front(p_mv_dcll_t plist, data_t new_data);
+
+extern ret_t mv_dcll_insert_front(p_mv_dcll_t plist,data_t new_data);
 
 /**
  * @function 	This function will add a new data at a specified INDEX in the list
@@ -110,7 +107,8 @@ extern ret_t mv_dcll_insert_front(p_mv_dcll_t plist, data_t new_data);
  * @returns		status of insertion
  * 				SUCCESS if successful / FAILURE if unsuccessful
  */
-extern ret_t mv_dcll_insert_at(p_mv_dcll_t plist, data_t new_data, size_t index);
+
+extern ret_t mv_dcll_insert_at(p_mv_dcll_t plist,data_t new_data,size_t index);
 
 /**
  * @function	This function will add a new data BEFORE an existing data
@@ -121,7 +119,8 @@ extern ret_t mv_dcll_insert_at(p_mv_dcll_t plist, data_t new_data, size_t index)
  * @returns		status of insertion
  * 				SUCCESS if successful / FAILURE if unsuccessful
  */
-extern ret_t mv_dcll_insert_before(p_mv_dcll_t plist, data_t new_data, data_t e_data, COMPAREDATAPROC  pcomparedataproc);
+
+extern ret_t mv_dcll_insert_before(p_mv_dcll_t plist,data_t new_data,data_t e_data,COMPAREDATAPROC  pcomparedataproc);
 
 /**
  * @function	This function will add a new data AFTER an existing data
@@ -132,7 +131,8 @@ extern ret_t mv_dcll_insert_before(p_mv_dcll_t plist, data_t new_data, data_t e_
  * @returns		status of insertion
  * 				SUCCESS if successful / FAILURE if unsuccessful
  */
-extern ret_t mv_dcll_insert_after(p_mv_dcll_t plist, data_t new_data, data_t e_data, COMPAREDATAPROC pcomparedataproc);
+
+extern ret_t mv_dcll_insert_after(p_mv_dcll_t plist,data_t new_data,data_t e_data,COMPAREDATAPROC pcomparedataproc);
 
 //	List Data Removal Functions
 /**
@@ -140,6 +140,7 @@ extern ret_t mv_dcll_insert_after(p_mv_dcll_t plist, data_t new_data, data_t e_d
  * @params		List
  * @returns		Removed data from the list OR NULL if no data.
  */
+
 extern data_t mv_dcll_remove_back(p_mv_dcll_t plist);
 
 /**
@@ -147,6 +148,7 @@ extern data_t mv_dcll_remove_back(p_mv_dcll_t plist);
  * @params		List
  * @returns		Removed data from the list OR NULL if no data.
  */
+
 extern data_t mv_dcll_remove_front(p_mv_dcll_t plist);
 
 /**
@@ -155,7 +157,8 @@ extern data_t mv_dcll_remove_front(p_mv_dcll_t plist);
  * 				2) INDEX
  * @returns		Removed data from the list OR NULL if no data.
  */
-extern data_t mv_dcll_remove_at(p_mv_dcll_t plist, size_t index);
+
+extern data_t mv_dcll_remove_at(p_mv_dcll_t plist,size_t index);
 
 /**
  * @function	This function will remove data BEFORE an existing data
@@ -164,7 +167,8 @@ extern data_t mv_dcll_remove_at(p_mv_dcll_t plist, size_t index);
  * 				3) Compare Callback function pointer
  * @returns		Removed data form the list OR NULL if no data.
  */
-extern data_t mv_dcll_remove_before(p_mv_dcll_t plist, data_t e_data, COMPAREDATAPROC pcomparedataproc);
+
+extern data_t mv_dcll_remove_before(p_mv_dcll_t plist,data_t e_data,COMPAREDATAPROC pcomparedataproc);
 
 /**
  * @function	This function will remove data AFTER an existing data
@@ -173,7 +177,8 @@ extern data_t mv_dcll_remove_before(p_mv_dcll_t plist, data_t e_data, COMPAREDAT
  * 				3) Compare Callback function pointer
  * @returns		Removed data from the list OR NULL is no data.
  */
-extern data_t mv_dcll_remove_after(p_mv_dcll_t plist, data_t e_data, COMPAREDATAPROC pcomparedataproc);
+
+extern data_t mv_dcll_remove_after(p_mv_dcll_t plist,data_t e_data,COMPAREDATAPROC pcomparedataproc);
 
 //	List Data Getter Functions
 
@@ -182,6 +187,7 @@ extern data_t mv_dcll_remove_after(p_mv_dcll_t plist, data_t e_data, COMPAREDATA
  * @params		List
  * @returns		Data of head of the list if present else NULL.
  */
+
 extern data_t mv_dcll_get_head(p_mv_dcll_t plist);
 
 /**
@@ -189,6 +195,7 @@ extern data_t mv_dcll_get_head(p_mv_dcll_t plist);
  * @params		List
  * @returns		Data of tail of the list if present else NULL.
  */
+
 extern data_t mv_dcll_get_tail(p_mv_dcll_t plist);
 
 /**
@@ -196,6 +203,7 @@ extern data_t mv_dcll_get_tail(p_mv_dcll_t plist);
  * @params		List
  * @returns		Size(No of elements)
  */
+
 extern size_t mv_dcll_get_size(p_mv_dcll_t plist);
 
 /**
@@ -204,6 +212,7 @@ extern size_t mv_dcll_get_size(p_mv_dcll_t plist);
  * @returns		TRUE is list present 
  * 				FALSE if list not present
  */
+
 extern bool_t mv_dcll_empty(p_mv_dcll_t plist);
 
 //	List Show Data Functions
@@ -213,7 +222,8 @@ extern bool_t mv_dcll_empty(p_mv_dcll_t plist);
  * 				2) Show Data Callback function pointer
  * @returns		Status of printing data
  */
-extern ret_t mv_dcll_print_forward(p_mv_dcll_t plist, SHOWDATAPROC pshowdataproc);
+
+extern ret_t mv_dcll_print_forward(p_mv_dcll_t plist,SHOWDATAPROC pshowdataproc);
 
 /**
  * @function	This function will print the data in reverse order
@@ -221,7 +231,8 @@ extern ret_t mv_dcll_print_forward(p_mv_dcll_t plist, SHOWDATAPROC pshowdataproc
  * 				2) Show Data Callback function pointer
  * @returns		Staus of printing data
  */
-extern ret_t mv_dcll_print_reverse(p_mv_dcll_t plist, SHOWDATAPROC pshowdataproc);
+
+extern ret_t mv_dcll_print_reverse(p_mv_dcll_t plist,SHOWDATAPROC pshowdataproc);
 
 /**
  * @function	This function will destroy the passed list
@@ -229,6 +240,6 @@ extern ret_t mv_dcll_print_reverse(p_mv_dcll_t plist, SHOWDATAPROC pshowdataproc
  * 				2) Destroy Data Callback function pointer
  * @returns		Status of detroying the list
  */
-extern ret_t mv_dcll_destroy(pp_mv_dcll_t pplist, DELETEDATAPROC pdeletedataproc);
 
+extern ret_t mv_dcll_destroy(pp_mv_dcll_t pplist,DELETEDATAPROC pdeletedataproc);
 #endif /* _MV_DCLL_H */
